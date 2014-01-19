fe439897bd52a4c81738613ed6b1bbb27bfc6577
Pranav Ravichandran <me@onloop.net>
2014-01-19 13:43:01 +0530

    Populatory commit

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from flask import Flask, request, Response, render_template
+import twitter
+from twitter_keys import consumer_key, consumer_secret, oauth_token, oauth_secret
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    if request.method == 'GET':
+        return 'OK'
+    if request.method == 'POST':
+        status = request.form['buyer_name'] + '(' + request.form['buyer'] + ') claimed ' + request.form['offer_title']
+        twitter_post(consumer_key, consumer_secret, oauth_token, oauth_secret, status)
+
+def twitter_post(consumer_key, consumer_secret, oauth_token, oauth_secret, post_status):
+    t = twitter.Twitter(auth = twitter.OAuth(oauth_token, oauth_secret, consumer_key, consumer_secret))
+    t.statuses.update(status = post_status)
